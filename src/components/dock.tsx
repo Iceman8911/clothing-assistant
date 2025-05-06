df132a3f08a519cb105c11b26442dfa421d5e772
Iceman8911 <wuchiJSS3@gmail.com>
2025-05-06 14:08:57 +0100

    wip: added clicking feedback :3

--- src/components/dock.tsx
+++ src/components/dock.tsx
@@ -2,23 +2,48 @@ import StockIcon from "lucide-solid/icons/blocks";
 import HomeIcon from "lucide-solid/icons/home";
 import ReportsIcon from "lucide-solid/icons/candlestick-chart";
 import SettingsIcon from "lucide-solid/icons/settings";
+import { createSignal } from "solid-js";
+
+const enum DockButton {
+	HOME,
+	STOCK,
+	REPORTS,
+	SETTINGS,
+}
 
 export default function Dock(props: any) {
+	const [activeBtn, setActiveBtn] = createSignal(DockButton.HOME);
+	const handleBtnClick = (btn: DockButton) => {
+		setActiveBtn(btn);
+	};
+
 	return (
 		<div class="dock">
-			<button class="dock-active">
+			<button
+				class={activeBtn() == DockButton.HOME ? "dock-active" : ""}
+				onClick={() => handleBtnClick(DockButton.HOME)}
+			>
 				<HomeIcon />
 				<span class="dock-label">Home</span>
 			</button>
-			<button>
+			<button
+				class={activeBtn() == DockButton.STOCK ? "dock-active" : ""}
+				onClick={() => handleBtnClick(DockButton.STOCK)}
+			>
 				<StockIcon />
 				<span class="dock-label">Stock</span>
 			</button>
-			<button>
+			<button
+				class={activeBtn() == DockButton.REPORTS ? "dock-active" : ""}
+				onClick={() => handleBtnClick(DockButton.REPORTS)}
+			>
 				<ReportsIcon />
 				<span class="dock-label">Reports</span>
 			</button>
-			<button>
+			<button
+				class={activeBtn() == DockButton.SETTINGS ? "dock-active" : ""}
+				onClick={() => handleBtnClick(DockButton.SETTINGS)}
+			>
 				<SettingsIcon />
 				<span class="dock-label">Settings</span>
 			</button>
