4a21b0208ab87713731fb60dd18285c3d6504757
Iceman8911 <wuchiJSS3@gmail.com>
2025-05-06 13:56:41 +0100

    wip: added lucide icons to the dock :3

--- src/components/dock.tsx
+++ src/components/dock.tsx
@@ -1,16 +1,25 @@
+import StockIcon from "lucide-solid/icons/blocks";
+import HomeIcon from "lucide-solid/icons/home";
+import ReportsIcon from "lucide-solid/icons/candlestick-chart";
+import SettingsIcon from "lucide-solid/icons/settings";
+
 export default function Dock(props: any) {
 	return (
 		<div class="dock">
 			<button class="dock-active">
+				<HomeIcon />
 				<span class="dock-label">Home</span>
 			</button>
 			<button>
+				<StockIcon />
 				<span class="dock-label">Stock</span>
 			</button>
 			<button>
+				<ReportsIcon />
 				<span class="dock-label">Reports</span>
 			</button>
 			<button>
+				<SettingsIcon />
 				<span class="dock-label">Settings</span>
 			</button>
 		</div>
