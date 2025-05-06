f0890d164c2c837917c9bd05d4c9f9cb3c1b87f6
Iceman8911 <wuchiJSS3@gmail.com>
2025-05-06 14:33:09 +0100

    wip: added a dark / light theme toggle!

--- src/components/dock.tsx
+++ src/components/dock.tsx
@@ -2,7 +2,9 @@ import StockIcon from "lucide-solid/icons/blocks";
 import HomeIcon from "lucide-solid/icons/home";
 import ReportsIcon from "lucide-solid/icons/candlestick-chart";
 import SettingsIcon from "lucide-solid/icons/settings";
-import { createSignal } from "solid-js";
+import SunIcon from "lucide-solid/icons/sun";
+import MoonIcon from "lucide-solid/icons/moon";
+import { createSignal, onMount } from "solid-js";
 
 const enum DockButton {
 	HOME,
@@ -16,6 +18,16 @@ export default function Dock(props: any) {
 	const handleBtnClick = (btn: DockButton) => {
 		setActiveBtn(btn);
 	};
+
+	const [prefersDarkTheme, setPrefersDarkTheme] = createSignal(false);
+
+	onMount(() => {
+		const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+		setPrefersDarkTheme(mediaQuery.matches);
+		mediaQuery.addEventListener("change", (event) => {
+			setPrefersDarkTheme(event.matches);
+		});
+	});
 
 	return (
 		<div class="dock">
@@ -46,6 +58,21 @@ export default function Dock(props: any) {
 			>
 				<SettingsIcon />
 				<span class="dock-label">Settings</span>
+			</button>
+			<button>
+				<label class="swap swap-rotate">
+					{/* this hidden checkbox controls the state */}
+					<input
+						type="checkbox"
+						class="theme-controller"
+						value={prefersDarkTheme() ? "light" : "dark"}
+					/>
+
+					<SunIcon class="swap-off" />
+
+					<MoonIcon class="swap-on" />
+				</label>
+				<span class="dock-label whitespace-nowrap">Toggle Theme</span>
 			</button>
 		</div>
 	);
