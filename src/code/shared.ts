536b8c7dd358ff4851cd02a835a5649008a82447
Iceman8911 <wuchiJSS3@gmail.com>
2025-05-10 17:24:58 +0100

    wip: added the input for setting cloudmersive API keys

--- src/code/shared.ts
+++ src/code/shared.ts
@@ -1,6 +1,8 @@
 import { createSignal } from "solid-js";
 import type { ClothingItem } from "./types";
 import { ReactiveMap } from "@solid-primitives/map";
+import { createStore } from "solid-js/store";
+import { useNavigate } from "@solidjs/router";
 
 /**
  * Each clothing item is indexed with it's `id`
@@ -11,3 +13,7 @@ export const gClothingItems = new ReactiveMap<string, ClothingItem>();
  * Global search text used for filtering
  */
 export const [gSearchText, gSetSearchText] = createSignal("");
+
+export const [gApiKeys, gSetApiKeys] = createStore({
+	cloudmersive: "",
+});
--- src/routes/settings.tsx
+++ src/routes/settings.tsx
@@ -1,7 +1,25 @@
+import { produce } from "solid-js/store";
+import { gApiKeys, gSetApiKeys } from "~/code/shared";
+
 export default function SettingsPage() {
 	return (
-		<>
-			<div>Settings</div>
-		</>
+		<div class="p-4">
+			<h2 class="font-bold">Settings</h2>
+			<fieldset class="fieldset">
+				<legend class="fieldset-legend">Cloudmersive API Key:</legend>
+				<input
+					type="text"
+					class="input"
+					placeholder="Type here"
+					value={gApiKeys.cloudmersive}
+					onfocusout={(e) =>
+						gSetApiKeys(
+							produce((state) => (state.cloudmersive = e.currentTarget.value))
+						)
+					}
+				/>
+				<p class="label">Optional</p>
+			</fieldset>
+		</div>
 	);
 }
