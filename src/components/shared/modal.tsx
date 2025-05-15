df380435e3d985059f531705ff852c10497a96c7
Iceman8911 <wuchiJSS3@gmail.com>
2025-05-15 15:08:05 +0100

    refactor: class changes to the modal

--- src/components/shared/modal.tsx
+++ src/components/shared/modal.tsx
@@ -7,28 +7,25 @@ export default function GenericModal(props: {
   stateSetter: Setter<boolean>;
 }) {
   return (
-    <>
-      <dialog
-        id="my_modal_2"
-        class="modal"
-        open={props.stateAccessor()}
-        onClose={(_) => {
-          props.stateSetter(false);
-        }}
-      >
-        <div class="modal-box">
-          <form method="dialog">
-            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-              ✕
-            </button>
-          </form>
-
-          {props.children}
-        </div>
-        <form method="dialog" class="modal-backdrop">
-          <button></button>
+    <dialog
+      class="modal modal-bottom sm:modal-middle"
+      open={props.stateAccessor()}
+      onClose={(_) => {
+        props.stateSetter(false);
+      }}
+    >
+      <div class="modal-box">
+        <form method="dialog">
+          <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
         </form>
-      </dialog>
-    </>
+
+        {props.children}
+      </div>
+      <form method="dialog" class="modal-backdrop">
+        <button></button>
+      </form>
+    </dialog>
   );
 }
