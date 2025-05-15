1b3603ff3d684e5a3e97d304b3652047a08e5daa
Iceman8911 <wuchiJSS3@gmail.com>
2025-05-15 13:00:19 +0100

    style: zed formatting

--- src/components/shared/alert-toast.tsx
+++ src/components/shared/alert-toast.tsx
@@ -9,10 +9,10 @@ import CopyIcon from "lucide-solid/icons/copy";
 import { generateRandomId } from "~/code/shared";
 
 interface Alert {
-	id: string;
-	status: "success" | "info" | "warning" | "error";
-	message: string;
-	duration?: number;
+  id: string;
+  status: "success" | "info" | "warning" | "error";
+  message: string;
+  duration?: number;
 }
 
 const alerts = new ReactiveMap<string, Alert>();
@@ -21,64 +21,64 @@ const alerts = new ReactiveMap<string, Alert>();
  * Please don't call this any where else, bar the main `app.tsx`
  */
 export function AlertToast() {
-	return (
-		<Portal>
-			<div class="toast toast-top z-[999]">
-				<For each={[...alerts.values()]}>
-					{(alert) => (
-						<div
-							class={
-								`alert alert-soft ` +
-								(alert.status == "success"
-									? "alert-success"
-									: alert.status == "info"
-									? "alert-info"
-									: alert.status == "warning"
-									? "alert-warning"
-									: "alert-error")
-							}
-							ref={(el) => {
-								// Remove the alert after a while
-								if (el) {
-									const timer = setTimeout(() => {
-										alerts.delete(alert.id);
-										clearTimeout(timer);
-									}, alert.duration);
-								}
-							}}
-							onClick={() => alerts.delete(alert.id)}
-						>
-							<Switch>
-								<Match when={alert.status == "success"}>
-									<SuccessIcon />
-								</Match>
-								<Match when={alert.status == "info"}>
-									<InfoIcon />
-								</Match>
-								<Match when={alert.status == "warning"}>
-									<WarningIcon />
-								</Match>
-								<Match when={alert.status == "error"}>
-									<ErrorIcon />
-								</Match>
-							</Switch>
-							<span
-								onClick={(e) => {
-									e.stopPropagation();
-								}}
-							>
-								{alert.message}{" "}
-								<CopyIcon
-									class="inline-block cursor-pointer"
-									onClick={() => navigator.clipboard.writeText(alert.message)}
-								/>
-							</span>
-						</div>
-					)}
-				</For>
-			</div>
-		</Portal>
-	);
+  return (
+    <Portal>
+      <div class="toast toast-top z-[999]">
+        <For each={[...alerts.values()]}>
+          {(alert) => (
+            <div
+              class={
+                `alert alert-soft ` +
+                (alert.status == "success"
+                  ? "alert-success"
+                  : alert.status == "info"
+                    ? "alert-info"
+                    : alert.status == "warning"
+                      ? "alert-warning"
+                      : "alert-error")
+              }
+              ref={(el) => {
+                // Remove the alert after a while
+                if (el) {
+                  const timer = setTimeout(() => {
+                    alerts.delete(alert.id);
+                    clearTimeout(timer);
+                  }, alert.duration);
+                }
+              }}
+              onClick={() => alerts.delete(alert.id)}
+            >
+              <Switch>
+                <Match when={alert.status == "success"}>
+                  <SuccessIcon />
+                </Match>
+                <Match when={alert.status == "info"}>
+                  <InfoIcon />
+                </Match>
+                <Match when={alert.status == "warning"}>
+                  <WarningIcon />
+                </Match>
+                <Match when={alert.status == "error"}>
+                  <ErrorIcon />
+                </Match>
+              </Switch>
+              <span
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+              >
+                {alert.message}{" "}
+                <CopyIcon
+                  class="inline-block cursor-pointer"
+                  onClick={() => navigator.clipboard.writeText(alert.message)}
+                />
+              </span>
+            </div>
+          )}
+        </For>
+      </div>
+    </Portal>
+  );
 }
 
 /**
@@ -87,19 +87,19 @@ export function AlertToast() {
  * @returns The ID of the alert
  */
 export function gTriggerAlert(
-	status: "success" | "info" | "warning" | "error",
-	message: string,
-	/**
-	 * Defaults to ~3 seconds
-	 */
-	duration = 3333
+  status: "success" | "info" | "warning" | "error",
+  message: string,
+  /**
+   * Defaults to ~3 seconds
+   */
+  duration = 3333,
 ) {
-	const id = generateRandomId();
-	alerts.set(id, {
-		id,
-		status,
-		message,
-		duration: status == "error" ? duration * 1.5 : duration,
-	});
-	return id;
+  const id = generateRandomId();
+  alerts.set(id, {
+    id,
+    status,
+    message,
+    duration: status == "error" ? duration * 1.5 : duration,
+  });
+  return id;
 }
