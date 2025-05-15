c3672bca5fa1808eebb131982b7402490d8258f7
Iceman8911 <wuchiJSS3@gmail.com>
2025-05-15 17:59:37 +0100

    wip: btn text change

--- src/routes/inventory.tsx
+++ src/routes/inventory.tsx
@@ -258,7 +258,7 @@ export default function InventoryPage() {
               >
                 <a>
                   <SquarePen />
-                  Edit
+                  View/Edit
                 </a>
               </li>
               <li>
