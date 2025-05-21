b66c430e1b63b5c3a7fc0a7e7ca4d7a443be2d32
Iceman8911 <wuchiJSS3@gmail.com>
2025-05-21 10:55:35 +0100

    chore: configure server preset to cloudflarePages

--- app.config.ts
+++ app.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	server: { preset: "bun" },
-	ssr: false,
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  server: { preset: "cloudflarePages" },
+  ssr: false,
 });
