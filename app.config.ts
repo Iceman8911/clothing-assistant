5a9c64baab71a9e9473cbe9afece1f219628873e
Iceman8911 <wuchiJSS3@gmail.com>
2025-05-22 09:06:52 +0100

    chore: add vite-bundle-analyzer

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "@solidjs/start/config";
+import { analyzer } from "vite-bundle-analyzer";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), analyzer({ analyzerMode: "static" })],
   },
   server: { preset: "cloudflarePages" },
   ssr: false,
