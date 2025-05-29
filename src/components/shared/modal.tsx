55787d912a9ae70fec99b88d7e9385033190d171
Iceman8911 <wuchiJSS3@gmail.com>
2025-05-29 19:26:01 +0100

    wip: used a Portal for the generic modal

--- src/components/shared/modal.tsx
+++ src/components/shared/modal.tsx
@@ -1,4 +1,5 @@
 import { JSX } from "solid-js";
+import { Portal } from "solid-js/web";
 import { SignalProps } from "~/code/types";
 
 export default function GenericModal(
@@ -7,24 +8,26 @@ export default function GenericModal(
   },
 ) {
   return (
-    <dialog
-      class="modal modal-bottom sm:modal-middle"
-      open={props.stateAccessor()}
-      onClose={(_) => {
-        props.stateSetter(false);
-      }}
-    >
-      <div class="modal-box">
-        <form method="dialog">
-          <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-            ✕
-          </button>
+    <Portal>
+      <dialog
+        class="modal modal-bottom sm:modal-middle"
+        open={props.stateAccessor()}
+        onClose={(_) => {
+          props.stateSetter(false);
+        }}
+      >
+        <div class="modal-box">
+          <form method="dialog">
+            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <div class="prose">{props.children}</div>
+        </div>
+        <form method="dialog" class="modal-backdrop">
+          <button></button>
         </form>
-        <div class="prose">{props.children}</div>
-      </div>
-      <form method="dialog" class="modal-backdrop">
-        <button></button>
-      </form>
-    </dialog>
+      </dialog>
+    </Portal>
   );
 }
