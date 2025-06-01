9c37533e91c26be49b0ca00972a67e1d87eb0d70
Iceman8911 <wuchiJSS3@gmail.com>
2025-06-01 19:22:03 +0100

    fix: didn't work :p

--- app.config.ts
+++ app.config.ts
@@ -63,9 +63,9 @@ export default defineConfig({
   },
   server: {
     preset: "cloudflare_pages",
-    rollupConfig: {
-      external: ["node:async_hooks"],
-    },
+    // rollupConfig: {
+    //   external: ["node:async_hooks",],
+    // },
     prerender: {
       crawlLinks: true,
     },
